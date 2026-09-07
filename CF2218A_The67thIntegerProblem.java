34b1b1abe4a654d0c7f25d5a3e36e7269f94f037
S-Rajkumar-07 <skrajkumar05092005@gmail.com>
2026-09-07 21:28:48 +0530

    add Codeforces 2218A solution

---
+++ CF2218A_The67thIntegerProblem.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CF2218A_The67thIntegerProblem {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int x = sc.nextInt();
+
+            if (x < 67) {
+                System.out.println(x + 1);
+            } else {
+                System.out.println(67);
+            }
+        }
+
+        sc.close();
+    }
+}
